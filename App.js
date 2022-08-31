027ca12d05ade83e6d2a91d935ab47ea0f96fce9
PhattaradolNoppaket <107970883+PhattaradolNoppaket@users.noreply.github.com>
2022-08-31 10:04:15 +0700

    310822

--- App.js
+++ App.js
@@ -1,74 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View, Button, TextInput } from 'react-native';
-
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React from 'react';
-
-
-function HomeScreen({ navigation, route }) {
-
-  React.useEffect(() => {
-    if (route.params?.post) {
-      //Post updated,do somethin with 'route.params.post'
-      //For example, sent the post to the server      
-    }
-  }, [route.params?.post]);
-
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button
-        title='Create Post'
-        onPress={() => navigation.navigate('CreatePost')}
-      />
-      <Text style={{ margin: 10 }}>Post:{route.params?.post}</Text>
-    </View>
-  );
-}
-
-function CreatePostScreen({ navigation, route }) {
-
-  const [postText, setPostText] = React.useState('');
-
-  return (
-    // use Fragment
-    <>
-      <TextInput
-        multiline
-        placeholder='Plese text here'
-        style={{ height: 200, padding: 10, backgroundColor: 'grey' }}
-        onChangeText={setPostText}
-        value={postText}
-      />
-      <Button
-        title='Click'
-        onPress={() => {
-          //Pass params back to HomeScreen funchion
-          navigation.navigate('Home', { post: postText })
-        }}
-      />
-    </>
-
-  );
-}
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+} from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import FirstPage from "./Pages/FirstPage";
+import SecondPage from "./Pages/SecondPage";
+import ThirdPage from "./Pages/ThirdPage";
 
 const Stack = createNativeStackNavigator();
 
-export default function App() {
+function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName='Home'
+        initialRouteName="First Page"
         screenOptions={{
-          headerStyle: { backgroundColor: 'magenta' },
-          headerTintColor: '#ffff',
-          headerTitleStyle: { fontWeight: 'bold' }
+          headerStyle: { backgroundColor: "blue" },
+          headerTintColor: "#fff",
+          headerTitleStyle: { fontWeight: "bold", fontSize: 24 },
         }}
       >
-        <Stack.Screen name='Home' component={HomeScreen} />
-        <Stack.Screen name='CreatePost' component={CreatePostScreen} />
+        <Stack.Screen name="First Page" component={FirstPage} />
+        <Stack.Screen name="Second Page" component={SecondPage} />
+        <Stack.Screen name="Third Page" component={ThirdPage} />
       </Stack.Navigator>
     </NavigationContainer>
-
   );
 }
+
+export default App;
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 20,
+  },
+  heading: {
+    fontSize: 25,
+    textAlign: "center",
+    marginVertical: 10,
+  },
+  textStyle: {
+    textAlign: "center",
+    fontSize: 16,
+    marginVertical: 10,
+  },
+});
