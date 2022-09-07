2e1f9df38eb43f389ca15c76acade04c2325382f
Phattaradol Noppaket <Copterhyperdragon@gmail.com>
2022-09-07 10:55:48 +0700

    099077222

--- App.js
+++ App.js
@@ -1,26 +1,26 @@
 import * as React from "react";
-import { Button, View, Text } from "react-native";
+import { Button, View, Text ,SafeAreaView,Image,StyleSheet} from "react-native";
 import {
   createDrawerNavigator,
   DrawerContentScrollView,
   DrawerItemList,
   DrawerItem,
 } from "@react-navigation/drawer";
-import { NavigationContainer ,DefaultTheme} from "@react-navigation/native";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import HomeScreen from "./screens/HomeScreen"
 
 const MyTheme = {
   ...DefaultTheme,
-  colors:{
+  colors: {
     ...DefaultTheme.colors,
-    primary:'rgb(255,45,85)',
+    primary: "rgb(255,45,85)",
     // background:'rgb(242,242,242)',
     // card:'rgb(255,255,255)',
     // text:'rgb(28,28,30)',
     // border:'rgb(199,199,204)',
     // notification:'rgb(255,69,58)',
-  }
-}
-
+  },
+};
 function Feed({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
@@ -30,29 +30,33 @@ function Feed({ navigation }) {
       /> */}
       <Text>Feed Screen</Text>
       <Button onPress={() => navigation.openDrawer()} title="Open Drawer" />
-      <Button onPress={() => navigation.toggleDrawer()} title="Toggle Drawer" />
     </View>
   );
 }
 
-function Article({ navigation }) {
+function Notifications({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       {/* <Button onPress={() => navigation.goBack()} title="Go back home" /> */}
-      <Text>Article Screen</Text>
+      <Text>Notifications</Text>
       <Button onPress={() => navigation.openDrawer()} title="Open Drawer" />
-      <Button onPress={() => navigation.toggleDrawer()} title="Toggle Drawer" />
     </View>
   );
 }
 
 function CustomDrawerContent(props) {
   return (
+    <SafeAreaView style={styles.container}>
+      
     <DrawerContentScrollView {...props}>
+    <Image source={require('./assets/react_logo.png')} style={styles.sideMenuProfileIcon}/>
       <DrawerItemList {...props} />
-      <DrawerItem label="Close Drawer" onPress={() => props.navigation.closeDrawer()} />
-      <DrawerItem label="Toggle Drawer" onPress={() => props.navigation.toggleDrawer()} />
+      <DrawerItem
+        label="Close Drawer"
+        onPress={() => props.navigation.closeDrawer()}
+      />
     </DrawerContentScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -63,25 +67,33 @@ function MyDrawer() {
     <Drawer.Navigator
       useLegacyImplementation
       drawerContent={(props) => <CustomDrawerContent {...props} />}
-      screenOptions = {
-        {
-          drawerStyle:{
-            // backgroundColor:'cyan',
-            width:240
-          }
-        }
-      }
+      screenOptions={{
+        drawerStyle: {
+          // backgroundColor:'cyan',
+          width: 240,
+        },
+      }}
     >
-      <Drawer.Screen name="Feed" component={Feed} />
-      <Drawer.Screen name="Article" component={Article} />
+      
+      <Drawer.Screen name="Home" component={HomeScreen} />
+      <Drawer.Screen name="Notifications" component={Notifications} />
     </Drawer.Navigator>
   );
 }
 
 export default function App() {
   return (
-    <NavigationContainer theme ={MyTheme}>
+    <NavigationContainer theme={MyTheme}>
       <MyDrawer />
     </NavigationContainer>
   );
 }
+const styles = StyleSheet.create({
+  sideMenuProfileIcon: {
+    resizeMode: "center",
+    width: 100,
+    height: 100,
+    borderRadius: 100 / 2,
+    alignSelf: "center",
+  },
+});
